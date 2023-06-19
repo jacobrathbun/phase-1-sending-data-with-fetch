54449bba00cbcca48adc89d75878042321aefcac
Jacob Rathbun <jrathofbun@gmail.com>
2023-06-19 18:07:20 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+    //formData for stringify later on
+    const formData = {
+        name: name,
+        email: email
+    };
+    //configurationObject for use in fetch later on
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    };
+    //fetch chain that sends data and handles responses and errors
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            const id = data.id;
+            document.body.innerHTML += `<p>New ID: ${id}</p>`;
+        })
+        .catch(error => {
+            document.body.innerHTML += `<p>Error: ${error}</p>`;
+        });
+} 
